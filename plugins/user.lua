c3b6823ee1424862db9fb9358e17b2f27d4d9fc6
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-06-07 21:13:25 +0800

    添加：neovide配置

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,4 @@
--- TODO Test
+-- todo Test
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -13,10 +13,8 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      signs = true,      -- show icons in the signs column
-      sign_priority = 8, -- sign priority
-    },
+    opts = {},
     event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
   },
 }
