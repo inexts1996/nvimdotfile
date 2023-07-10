a3c63abd5d351e712011be7cce734507689fe5e8
zhouzigui <zhouzigui@huixuanjiasu.com>
2023-07-10 20:30:50 +0800

    config neovide

--- plugins/neovide.lua
+++ plugins/neovide.lua
@@ -2,8 +2,11 @@ local function setUp()
   if vim.g.neovide then
     vim.g.neovide_cursor_vfx_mode = "wireframe"
     vim.api.nvim_set_var("neovide_refresh_rate", 75)
-    vim.o.guifont = "Hack NFM:h12"
-    vim.g.neovide_fullscreen = true
+    vim.o.guifont = "Hack NFM:h11"
+    vim.g.neovide_padding_top = 5
+    vim.g.neovide_padding_bottom = 5
+    vim.g.neovide_padding_right = 5
+    vim.g.neovide_padding_left = 5
     vim.g.neovide_remember_window_size = true
   end
 end
